d0b0e49766b1bf77a0d61c6bcb0b1c862698dda3
Oleksandr Putsyk <sashaputsyk100903@gmail.com>
2024-03-02 14:44:45 +0200

    Initial Commit

---
+++ Blog.Web/Data/AppEFContext.cs
@@ -0,0 +1,25 @@
+﻿using Blog.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Data
+{
+    public class AppEFContext : DbContext
+    {
+        public AppEFContext(DbContextOptions<AppEFContext> options)
+            : base(options)
+        { }
+        public DbSet<CategoryEntity> Categories { get; set; }
+        public DbSet<TagEntity> Tags { get; set; }
+        public DbSet<PostEntity> Posts { get; set; }
+        public DbSet<PostTagEntity> PostTags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<PostTagEntity>(ur =>
+            {
+                ur.HasKey(ur => new { ur.PostId, ur.TagId });
+            });
+        }
+    }
+}
---
+++ Blog.Web/Data/Entities/TagEntity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Web.Data.Entities
+{
+    [Table("tblTags")]
+    public class TagEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+
+        [Required, StringLength(50)]
+        public string UrlSlug { get; set; }
+
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        public virtual ICollection<PostTagEntity> PostTags { get; set; }
+    }
+}
